bb837f2b6a23868a3b3cf7c14f333a064aa5db7a
unknown <alexhahn@vt.edu>
2017-11-16 09:00:59 -0500

    Satisfaction constraints

--- DANK_ScheduleDSS/Optimization.vb
+++ DANK_ScheduleDSS/Optimization.vb
@@ -28,10 +28,18 @@ Public Class Optimization
         Dim constraintKey As String
         Dim constraintIndex As Integer
 
-        'constraintKey = "Satisfaction"
-        'For Each course As Course In CreateObjects.CourseList
-
-        'Next
+        'Create satisfaction constraints
+        For Each section As Section In CreateObjects.SectionList
+            constraintKey = "Satisfaction Constraint: " & section.Section
+            Solver.AddRow(constraintKey, constraintIndex)
+            For Each course As Course In CreateObjects.CourseList
+                coefficient = CreateObjects.SectionList(0).Section
+                dvKey = course.CRN
+                dvIndex = Solver.GetIndexFromKey(dvKey)
+                Solver.SetCoefficient(constraintIndex, dvIndex, coefficient)
+            Next
+            Solver.SetBounds(constraintIndex, 0, Rational.PositiveInfinity)
+        Next
 
         'Create Overlap constraints
         For Each period As Period In CreateObjects.PeriodList
@@ -45,34 +53,6 @@ Public Class Optimization
             Next
             Solver.SetBounds(constraintIndex, 0, 1)
         Next
-
-        ''Capacity Constraints
-        'For Each capacity As Capacity In IC5CreateObjects.CapacityList
-        '    constraintKey = "Capacity Constraint_" & capacity.DepartmentName
-        '    Solver.AddRow(constraintKey, constraintIndex)
-        '    For Each activity As Activity In CreateObjects.ActivityList
-        '        coefficient = 1
-        '        dvKey = "BIT_Activity 1"
-        '        dvIndex = IC5Solver.GetIndexFromKey(dvKey)
-        '        IC5Solver.SetCoefficient(constraintIndex, dvIndex, coefficient)
-        '    Next
-        '    Solver.SetBounds(constraintIndex, 0, capacity.Capacity)
-
-        'Next
-
-        ''Acitivty Coverage Constraints
-        'For Each activity As Activity In IC5CreateObjects.ActivityList
-        '    constraintKey = "Activity Constraint_" & activity.ActivityName
-        '    IC5Solver.AddRow(constraintKey, constraintIndex)
-        '    For Each capacity As Capacity In IC5CreateObjects.CapacityList
-        '        coefficient = 1
-        '        dvKey = "BIT_Activity 1"
-        '        dvIndex = IC5Solver.GetIndexFromKey(dvKey)
-        '        IC5Solver.SetCoefficient(constraintIndex, dvIndex, coefficient)
-        '    Next
-        '    IC5Solver.SetBounds(constraintIndex, 0, Capacity.Capacity)
-
-        'Next
 
         ''Define the objective
         'Dim objKey As String = "Objective Function"
